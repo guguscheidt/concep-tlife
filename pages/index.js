f8433d4c2fbce82ac5e3c3b2ed44df40d06e2e09
guguscheidt <gustavoc.scheidt@gmail.com>
2023-05-09 17:47:52 -0300

    pages/promocoes.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function home() {
-    return <div>home</div>
+    return (<div>
+       <h1>home</h1> 
+       <a href="/promocoes">acessar promocoes</a>
+       </div>)
+    
 }
 
 export default home 
